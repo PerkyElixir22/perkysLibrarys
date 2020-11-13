f044510480c92dde186811ae71e712c29163017e
PerkyElixir22 <64582251+PerkyElixir22@users.noreply.github.com>
2020-11-13 16:23:34 +0100

    fixed screenLib.lua

--- lib/screenLib.lua
+++ lib/screenLib.lua
@@ -4,9 +4,9 @@ local function rgb(r, g, b)
   r = tonumber(r)
   g = tonumber(g)
   b = tonumber(b)
-  if r = nil then r = 0 end
-  if g = nil then g = 0 end
-  if b = nil then b = 0 end
+  if r == nil then r = 0 end
+  if g == nil then g = 0 end
+  if b == nil then b = 0 end
   if r > 256 then r = 256 end
   if g > 256 then g = 256 end
   if b > 256 then b = 256 end
@@ -21,7 +21,7 @@ end
 
 local function clearScreen(color)
   color = tonumber(color)
-  if color = nil then color = 0 end
+  if color == nil then color = 0 end
   if color < 0 then color = 0 end
   if color > 16843008 then color = 16843008 end
   local gpu = require("component").gpu
@@ -37,8 +37,8 @@ local function centreText(text, y)
   local w, h = gpu.getResolution()
   text = tostring(text)
   y = tonumber(y)
-  if text = nil then text = " " end
-  if y = nil then y = 1 end
+  if text == nil then text = " " end
+  if y == nil then y = 1 end
   if #text > w then text = string.sub(text, 1, w-#text) end
   gpu.set(w/2-#text/2, text)
 end
