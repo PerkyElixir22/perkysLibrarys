6afd54d5dd74aefe86e612b69c78e8cc5e6155f1
PerkyElixir22 <64582251+PerkyElixir22@users.noreply.github.com>
2020-11-13 17:39:32 +0100

    Fixed RGB to HEX conversion

--- lib/screenLib.lua
+++ lib/screenLib.lua
@@ -7,14 +7,14 @@ local function rgb(r, g, b)
   if r == nil then r = 0 end
   if g == nil then g = 0 end
   if b == nil then b = 0 end
-  if r > 256 then r = 256 end
-  if g > 256 then g = 256 end
-  if b > 256 then b = 256 end
+  if r > 255 then r = 255 end
+  if g > 255 then g = 255 end
+  if b > 255 then b = 255 end
   if r < 0 then r = 0 end
   if g < 0 then g = 0 end
   if b < 0 then b = 0 end
   r = r*65536
-  g = g*256
+  g = g*255
   b = b*1
   return r+g+b
 end
